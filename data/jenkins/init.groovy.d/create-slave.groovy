2148e860a1bc6231f95fe32001e4407c72df6b28
Zougi <zougi.fr@gmail.com>
2018-04-17 20:13:01 +0200

    some tries later..

--- data/jenkins/init.groovy.d/create-slave.groovy
+++ data/jenkins/init.groovy.d/create-slave.groovy
@@ -6,27 +6,20 @@ import jenkins.model.*
 import hudson.slaves.*
 import hudson.model.Node.Mode
 
-def slaveName = 'jenkins_node_jnlp'
+def slave = [
+    name: 'jenkins_node_jnlp',
+    description: 'some slave hosted with docker',
+    workspace: '/home/jenkins/',
+    buildExecutors: 8,
+    labels: 'linux_x64'
+]
 
-DumbSlave dumb = new DumbSlave(
-    //name
-    slaveName,
-    //description
-    'some slave hosted with docker',
-    //workspace
-    '/home/jenkins/',
-    '8',
-    Mode.EXCLUSIVE,
-    // Labels
-    'linux_x64',
-    new JNLPLauncher(),
-    RetentionStrategy.INSTANCE,
-    new LinkedList()
-)
+DumbSlave dumb = new DumbSlave(slave.name, slave.description, slave.workspace, "${slave.buildExecutors}",
+    Mode.NORMAL, slave.labels, new JNLPLauncher(), RetentionStrategy.INSTANCE, new LinkedList())
 Jenkins.instance.addNode(dumb)
 
 // write secret to jenkins_home
-def slaveSecret = Jenkins.instance.getNode(slaveName)?.computer?.jnlpMac
+def slaveSecret = Jenkins.instance.getNode(slave.name)?.computer?.jnlpMac
 def scriptDir = new File(new File(getClass().protectionDomain.codeSource.location.path).parent).parent
 def slaveSecretFile = new File("${scriptDir}/slave.secret")
 slaveSecretFile.newWriter().withWriter { w ->
